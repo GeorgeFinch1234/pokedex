e0c6612bfd00da1fd435a3a4178b9650c11bb195
George Finch <georgefinch12@gmail.com>
2025-05-30 15:47:19 +0100

    cosmetic changes.
    Added a background image to body.
    Added a background image for the main div.

--- pokedex.js
+++ pokedex.js
@@ -8,13 +8,16 @@ let pokemon;
 
 
 async function getData() {
+    let pokemonIndex=1;
     fetch("https://pokeapi.co/api/v2/pokemon?limit=300&offset=0").then(Response => Response.json())
     .then(data=>{
+        console.log(data)
         data.results.forEach(element => {
             console.log(element.name)
            pokemon = document.createElement(`p`)
-           pokemon.textContent = (element.name)
+           pokemon.textContent = (pokemonIndex  + ". " + element.name)
            pokemonContainer.appendChild(pokemon);
+            pokemonIndex = pokemonIndex +1;
         });
     })
 
